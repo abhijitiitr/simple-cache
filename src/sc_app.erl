42098d44c7141e905c36794cb7eb2b315eba5fa7
Abhijit <singh.abhijitpratap@gmail.com>
2014-07-13 17:25:55 +0530

    towards a release

--- src/sc_app.erl
+++ src/sc_app.erl
@@ -1,13 +1,60 @@
 -module(sc_app).
 -behaviour(application).
--export([start/2, stop/1]).
+-export([start/2, stop/1,ensure_contact/0,ensure_contact/1]).
 start(_StartType, _StartArgs) ->
-	sc_store:init(),
-	case sc_sup:start_link() of 
-       {ok, Pid} ->
-           {ok, Pid};
-       Error ->
-           Error
-end.
+  ensure_contact(),
+  sc_resource_discovery:add_local_resource(simple_cache, node()),
+  sc_resource_discovery:add_target_resource_type(simple_cache),
+  sc_resource_discovery:trade_resources(),
+  timer:sleep(4000),
+  sc_store:init(),
+  case sc_sup:start_link() of 
+    {ok, Pid} ->
+      {ok, Pid};
+    Error ->
+      Error
+  end.
 stop(_State) ->
-   ok.
+  ok.
+
+
+ensure_contact() -> 
+  case get_env(simple_cache, contact_nodes, []) of
+    {ok, []} ->
+      error_logger:info_msg("no contact nodes~n");
+    {ok, ContactNodes} ->
+      ok = ensure_contact(ContactNodes),
+      {ok, WaitTime} = get_env(simple_cache, wait_time, 6000),
+      wait_for_nodes(length(ContactNodes), WaitTime)
+  end.
+
+ensure_contact([Node|T]) ->
+  case net_adm:ping(Node) of
+    pong ->
+      lists:foreach(fun(N) -> net_adm:ping(N) end, T);
+    pang ->
+      ensure_contact(T)
+  end;
+
+ensure_contact([]) ->
+  {error, no_contact_nodes_reachable}.
+
+wait_for_nodes(ContactNodes, WaitTime) ->
+  wait_for_nodes(ContactNodes, round(WaitTime / 3), 3).
+
+wait_for_nodes(_, _, 0) ->
+  ok;
+
+wait_for_nodes(ContactNodes, WaitSlice, Iterations) ->
+  case length(nodes()) > length(ContactNodes) of
+    true -> ok;
+    false ->
+      timer:sleep(WaitSlice),
+      wait_for_nodes(ContactNodes, WaitSlice, Iterations - 1)
+  end.
+
+get_env(AppName, Key, Default) ->
+  case application:get_env(AppName, Key) of
+    undefined -> {ok, Default};
+    Found     -> Found
+  end.
--- src/sc_store.erl
+++ src/sc_store.erl
@@ -1,21 +1,67 @@
 -module(sc_store).
 -export([
-        init/0,
-        insert/2,
-        delete/1,
-        lookup/1
-       ]).
+  init/0,
+  insert/2,
+  delete/1,
+  lookup/1
+  ]).
+
+-record(key_to_pid, {key, pid}).
+
 -define(TABLE_ID, ?MODULE).
+
 init() ->
-	ets:new(?TABLE_ID, [public, named_table]),
-	ok.
-insert(Key, Pid) ->
-   ets:insert(?TABLE_ID, {Key, Pid}).
+  {ok, CacheNodes} = sc_resource_discovery:fetch_resources(simple_cache),
+  dynamic_db_init(lists:delete(node(), CacheNodes)).
+%%mnesia:create_schema([node()]),
+%%    mnesia:start(),
+%%    mnesia:create_table(key_to_pid, [{index, [pid]}, {attributes, record_info(fields, key_to_pid)}]).
+
+insert(Key, Pid) when is_pid(Pid) ->
+  mnesia:write(#key_to_pid{key = Key, pid = Pid}).
+
 lookup(Key) ->
-   case ets:lookup(?TABLE_ID, Key) of
-       [{Key, Pid}] -> {ok, Pid};
-       []           -> {error, not_found}
-   end.
+  Fun = fun() ->
+      [{key_to_pid, Key, Pid}] = mnesia:read(key_to_pid, Key),
+      Pid end,
+  case mnesia:transaction(Fun) of
+    {atomic, Pid}      -> {ok, Pid};
+    {aborted, _Reason} -> {error, not_found}
+  end.
 
 delete(Pid) ->
-    ets:match_delete(?TABLE_ID, {'_', Pid}).
+  try
+    [#key_to_pid{} = Record] = mnesia:dirty_index_read(key_to_pid, Pid, #key_to_pid.pid), 
+    mnesia:dirty_delete_object(Record)
+  catch
+    _C:_E -> ok
+  end.
+
+dynamic_db_init([]) -> 
+  delete_schema(),
+  mnesia:create_schema([node()]),
+  mnesia:create_table(key_to_pid, [{index, [pid]},{attributes,record_info(fields, key_to_pid)}]);
+
+dynamic_db_init(CacheNodes) -> 
+  delete_schema(),
+  add_extra_nodes(CacheNodes).
+%% deletes a local schema.
+
+delete_schema() ->
+  mnesia:stop(),
+  mnesia:delete_schema([node()]),
+  mnesia:start().
+
+add_extra_nodes([Node|T]) ->
+  case mnesia:change_config(extra_db_nodes, [Node]) of 
+        {ok, [Node]} ->
+      Res = mnesia:add_table_copy(schema, node(), ram_copies), 
+      error_logger:info_msg("remote_init schema type ~p~n", [Res]),
+      Res1 = mnesia:add_table_copy(key_to_pid, node(), ram_copies), 
+      error_logger:info_msg("remote_init add_table copy = ~p~n",
+                            [Res1]),
+      Tables = mnesia:system_info(tables), 
+      mnesia:wait_for_tables(Tables, 3000);
+    _ ->
+      add_extra_nodes(T)
+  end.
